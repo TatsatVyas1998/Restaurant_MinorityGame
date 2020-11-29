b0ceec0ad022288a01b68b498c4d569408e6a44a
Tatsat Vyas <thv1@scarletmail.rutgers.edu>
2020-11-28 23:47:38 -0500

    still needs to finish the simmulation. Win and loose has been inmplelented and needs more algorithm thinking

--- Agent_strgy/methods.py
+++ Agent_strgy/methods.py
@@ -6,22 +6,22 @@ import random
 Mem = 5
 def pram_scale(Input_param):
 
-    Input_param['weather_condition'] = ((Input_param['weather_condition']*2)/10) - 0.01
+    Input_param['weather_condition'] = ((Input_param['weather_condition']*2)/10)
     Input_param['restaurant_capacity'] = Input_param['restaurant_capacity']/100
     Input_param['rate_of_spread'] = ((Input_param['rate_of_spread'])/3.5)    #3.5 is the max rate of spread reproted so far
 
 
 def get_random_weight(Input_param):
 
-    w1 = Input_param['weather_condition']* (random.randint(1,100 - int((Input_param['weather_condition']*100)))/16)
+    w1 = Input_param['weather_condition']* (random.randint(50,100 )/20)
     #print('weather_condition',w1)
-    w2 = Input_param['restaurant_capacity']* (random.randint(1, int(Input_param['restaurant_capacity']*100))/100)
-    #print('rate_of_spred',Input_param['rate_of_spread'])
+    w2 = Input_param['restaurant_capacity']* (random.randint(50, 100)/100)
+    #print('restaurant_capacity',w2)
     #print('w3', int(2+ Input_param['rate_of_spread']*100))
-    w3 = Input_param['rate_of_spread']*(random.randint(1,int(1+ Input_param['rate_of_spread']*100))/100)
+    w3 = Input_param['rate_of_spread']*(random.randint(60,100)/100)#int(1+ Input_param['rate_of_spread']*100))/100)
     #print('rate_of_spread',w3)
-    w4 = Input_param['un_employment_rate']*(random.randint(1,int(1+Input_param['un_employment_rate']*100))/100)
-
+    w4 = Input_param['un_employment_rate']*(random.randint(1,100)/100)
+    #print('un_employment_rate',w4)
     return ((w1+w2-w3-w4)/2)
 
 def compute_random_strgy(NUM_STRGY, Input_param):
@@ -45,4 +45,49 @@ def compute_agent_strgy(NUM_STRGY , Input_param):
 
 
 def compute_random_mem(population):
-    return[random.randint(10,population) for _ in range(0,Mem) ]
+    return[random.randint(40,population) for _ in range(0,Mem) ]
+
+def compute_thrshold(NUM_RESTAURANTS, AVG_RESTAURANT_CAP, Input_param):
+    #print(rst_capacity)
+    return  int(Input_param['num_agents']*.5) # (NUM_RESTAURANTS*AVG_RESTAURANT_CAP * ((Input_param['restaurant_capacity']*100)/100))
+
+def compute_agent_decision(agents, Global_mem ,thrs_hold):
+    agent_decisions = []
+    num_going = 0
+    num_notgoing = 0
+    for agent in agents:
+        opt_strgy = agent.strgy[agent.top_strgy]
+        #print(opt_strgy)
+        expected_turnout = 0
+        for i in range(0,len(opt_strgy)):
+            expected_turnout += opt_strgy[i]*Global_mem[i]
+
+
+        #print("expected turnout" ,int(expected_turnout*(1/len(opt_strgy))))
+        #print("threshold", thrs_hold)
+        if( int(expected_turnout*(1/len(opt_strgy))) < thrs_hold):
+            agent_decisions.append(1)
+            num_going +=1
+        else:
+            agent_decisions.append(0)
+            num_notgoing +=1
+    return agent_decisions , num_going ,num_notgoing
+
+def get_winner_loosers( agent_decision, num_going, num_notgoing):
+    not_g = False
+    g = False
+    win_loose_arr =[]
+    if( num_notgoing > num_going):
+        g = True
+    else:
+        not_g = True
+    if( not_g is True):
+        for i in range(0,len(agent_decision)):
+            if( agent_decision[i]==0):
+                win_loose_arr.append(1)
+            else:
+                win_loose_arr.append(0)
+
+    if(g==True):
+        win_loose_arr = agent_decision.copy()
+    return(win_loose_arr)
--- Agent_strgy/start.py
+++ Agent_strgy/start.py
@@ -1,15 +1,23 @@
 import numpy as np
 import methods as mtd
 import random
-INPUT_PARAMS = {'weather_condition' : 5 , 'rate_of_spread': 0.8 , 'restaurant_capacity' : 25 ,'un_employment_rate': 0.8,  'num_agents' : 100}
+INPUT_PARAMS = {'weather_condition' : 1 , 'rate_of_spread': .5 , 'restaurant_capacity' : 100 ,'un_employment_rate': 0.1,  'num_agents' : 100 , 'num_rounds' : 20}
 NUM_STRGY = 10
-population= 1000
+NUM_RESTAURANTS = 60 #Estimated number of rastaurants in the are
+AVG_RESTAURANT_CAP= 40 #average restaurant capacity in the area
+population= 100
 class agent:
     def __init__(self, strgy):
         self.strgy = strgy
         self.top_strgy = random.randint(0,9) #giving random strategy the best strategy
+        self.top_strgy_score = 0
     def best_strgy(idx):
         self.top_strgy = idx
+    def increase_top_score():
+        self.top_strgy_score +=1
+    def decrease_top_score():
+        self.top_strgy_score = max(0,self.top_strgy_score-1)
+
 
 class strgy:
     """docstring for ."""
@@ -24,6 +32,16 @@ class strgy:
 
 if __name__ == "__main__":
 #this is where the execution starts
-    agents = mtd.compute_agent_strgy( NUM_STRGY , INPUT_PARAMS)
+    agents = mtd.compute_agent_strgy( NUM_STRGY , INPUT_PARAMS )
     Global_mem = mtd.compute_random_mem(population)#redomly generated memory at the begining
-    
+    thrs_hold = mtd.compute_thrshold(NUM_RESTAURANTS, AVG_RESTAURANT_CAP, INPUT_PARAMS)
+    for _ in range(0,INPUT_PARAMS['num_rounds']):
+        agent_decision , num_going , num_notgoing = mtd.compute_agent_decision(agents, Global_mem ,thrs_hold)
+        #supply agent decisions to sub-team one and obtain the actual agent_decisions
+        #you store the num_goint here( it is the variable that tell you how many agents have decided to go out this round)
+        winner_loser = mtd.get_winner_loosers( agent_decision, num_going, num_notgoing)
+        
+
+    #print(agent_decision)
+    #print(winner_loser)
+    #print(Global_mem)
